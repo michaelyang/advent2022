b63699d1e9488e1238b42b510c73fd594d18c857
Michael <myopia92@gmail.com>
2022-12-21 02:51:46 -0500

    16

--- 16.py
+++ 16.py
@@ -4,110 +4,148 @@ INPUT_FILE_NAME = "input_16.txt"
 from typing import Tuple
 
 
-class Cave:
-    def __init__(self, column_number: int, row_number: int):
-        self.rows: list[list] = [["."] * column_number for _ in range(row_number)]
+class Valve:
+    def __init__(self, name: str, flow_rate: int):
+        self.name: str = name
+        self.flow_rate: int = flow_rate
+        self.connected_valve: list["str"] = []
 
-    def add_row(self, row: list):
-        self.rows.append(row)
+    def add_connected_valves(self, valves: "list[str]") -> None:
+        self.connected_valve.extend(valves)
 
-    def get_rows(self) -> "list[list]":
-        return self.rows
-
-    def get_columns(self) -> "list[list]":
-        return list(map(list, zip(*self.rows)))
-
-    def get_item(self, column, row):
-        return self.rows[row][column]
-
-    def set_item(self, column, row, item):
-        self.rows[row][column] = item
-
-    def fill_rock(self, path: "list[Tuple[int, int]]"):
-        last_column, last_row = path[0]
-        for point in path[1:]:
-            current_column, current_row = point
-            if last_column == current_column:
-                # fill Y
-                diff = abs(current_row - last_row)
-                for i in range(diff + 1):
-                    self.set_item(current_column, min(current_row, last_row) + i, "#")
-            elif last_row == current_row:
-                # fill X
-                diff = abs(current_column - last_column)
-                for i in range(diff + 1):
-                    self.set_item(
-                        min(current_column, last_column) + i, current_row, "#"
-                    )
-            last_column, last_row = current_column, current_row
-
-    def get_minimum_row_for_each_column(self) -> dict:
-        minimum_row_dict: dict = dict()
-        for i, column in enumerate(self.get_columns()):
-            minimum_row = None
-            for j, row in enumerate(column):
-                if row == "#":
-                    minimum_row = j
-            minimum_row_dict[i] = minimum_row
-        return minimum_row_dict
-
-    def add_bottom(self):
-        minimum_row: int = None
-        for column in self.get_columns():
-            for j, row in enumerate(column):
-                if row == "#":
-                    if not minimum_row or j > minimum_row:
-                        minimum_row = j
-        for column in range(len(self.rows[0])):
-            self.set_item(column, minimum_row + 2, "#")
-
-    def simulate_single_sand_fall(self, minimum_row_dict: dict) -> bool:
-        sand_column, sand_row = (500, 0)
-        if self.get_item(sand_column, sand_row) != ".":
-            return False
-        while (
-            minimum_row_dict[sand_column] and sand_row < minimum_row_dict[sand_column]
-        ):
-            # check below
-            below = self.get_item(sand_column, sand_row + 1)
-            diagonally_left = self.get_item(sand_column - 1, sand_row + 1)
-            diagonally_right = self.get_item(sand_column + 1, sand_row + 1)
-            if below == ".":
-                sand_row += 1
-            elif diagonally_left == ".":
-                sand_column -= 1
-                sand_row += 1
-            elif diagonally_right == ".":
-                sand_column += 1
-                sand_row += 1
-            else:
-                self.set_item(sand_column, sand_row, "O")
-                return True
-        return False
-
-    def simulate_sand_fall(self) -> int:
-        minimum_row_dict = self.get_minimum_row_for_each_column()
-        total_count = 0
-        while self.simulate_single_sand_fall(minimum_row_dict):
-            total_count += 1
-        return total_count
-
-    def __repr__(self):
-        return "\n".join("".join(str(item) for item in row) for row in self.rows)
+    def __repr__(self) -> str:
+        return "Valve {} has flow rate={}; tunnels lead to valves {}".format(
+            self.name, str(self.flow_rate), ", ".join(self.connected_valve)
+        )
 
 
 class Solver:
-    def parse_file(self, input_file_name: str) -> Cave:
-        cave = Cave(1000, 1000)
+    def parse_file(self, input_file_name: str) -> dict:
+        valve_dict = dict()
         with open(input_file_name) as f:
             for line in f:
                 stripped_line: str = line.strip()
-                point_raw_list = stripped_line.split(" -> ")
-                path: "list[Tuple[int, int]]" = []
-                for point_raw in point_raw_list:
-                    path.append(tuple(int(point) for point in point_raw.split(",")))
-                cave.fill_rock(path)
-        return cave
+                valve_raw, connection_raw = stripped_line.split(";")
+                name_raw, flow_rate_raw = (
+                    valve_raw.replace("Valve ", "")
+                    .replace(" has flow rate", "")
+                    .split("=")
+                )
+                connections = (
+                    connection_raw.replace(" tunnels lead to valves ", "")
+                    .replace(" tunnel leads to valve ", "")
+                    .split(", ")
+                )
+                valve = Valve(name_raw, int(flow_rate_raw))
+                valve.add_connected_valves(connections)
+                valve_dict[name_raw] = valve
+        return valve_dict
+
+    def get_highest_pressure_release(self, valve_dict: dict, minutes=26) -> int:
+        max_released_pressure = None
+        non_zero_flow_rate = 0
+        for valve in valve_dict.values():
+            if valve.flow_rate > 0:
+                non_zero_flow_rate += 1
+        # dict key to value
+        # key is valve, value is max
+
+        valve_set_dict = dict()
+
+        def visit_valve(
+            valve: Valve,
+            previous_valve: str,
+            released_pressure: int,
+            current_minute: int,
+            opened_valves: set,
+            valve_set_dict: dict,
+        ):
+            nonlocal max_released_pressure
+            nonlocal minutes
+            nonlocal non_zero_flow_rate
+
+            # print("In {} at {}".format(valve.name, current_minute))
+            if current_minute > minutes - 1 or len(opened_valves) >= non_zero_flow_rate:
+                return valve_set_dict
+            # open the current valve
+            if valve.name not in opened_valves and valve.flow_rate > 0:
+                opened_valves.add(valve.name)
+                minute_after_opening = current_minute + 1
+                new_released_pressure = (
+                    released_pressure
+                    + (minutes - minute_after_opening) * valve.flow_rate
+                )
+                """
+                print(
+                    "Opening {} for {}".format(
+                        valve.name,
+                        released_pressure,
+                    )
+                )"""
+                if (
+                    max_released_pressure is None
+                    or new_released_pressure >= max_released_pressure
+                ):
+                    max_released_pressure = new_released_pressure
+                frozen_set = frozenset(opened_valves.copy())
+                if (
+                    frozen_set in valve_set_dict
+                    and new_released_pressure > valve_set_dict[frozen_set]
+                ):
+                    valve_set_dict[frozen_set] = new_released_pressure
+                else:
+                    valve_set_dict[frozen_set] = new_released_pressure
+                visit_valve(
+                    valve,
+                    "",
+                    new_released_pressure,
+                    minute_after_opening,
+                    opened_valves,
+                    valve_set_dict,
+                )
+                opened_valves.remove(valve.name)
+            # visit connected valve
+            valves_to_visit = list(
+                filter(
+                    lambda valve: valve != previous_valve,
+                    valve.connected_valve,
+                ),
+            )
+            for valve_to_visit in valves_to_visit:
+                visit_valve(
+                    valve_dict[valve_to_visit],
+                    valve.name,
+                    released_pressure,
+                    current_minute + 1,
+                    opened_valves,
+                )
+            return valve_set_dict
+
+        visit_valve(valve_dict["AA"], "", 0, 0, set(), valve_set_dict)
+        print(valve_set_dict)
+        return max_released_pressure, valve_set_dict
+
+    def get_highest_pressure_release_2(self, valve_dict: dict, minutes=26) -> int:
+        _, dict = self.get_highest_pressure_release(valve_dict, minutes)
+        complete_set = set()
+        for key, valve in valve_dict.items():
+            if valve.flow_rate > 0:
+                complete_set.add(key)
+        highest_pressure_release = None
+        for key, value in dict.items():
+            remaining_set = complete_set.difference(key)
+            if remaining_set:
+                total_pressure_release = dict[frozenset(remaining_set)] + value
+            else:
+                total_pressure_release = value
+            if (
+                highest_pressure_release is None
+                or total_pressure_release >= highest_pressure_release
+            ):
+                print("higher")
+                highest_pressure_release = total_pressure_release
+
+        return highest_pressure_release
 
 
 import unittest
@@ -116,32 +154,34 @@ import unittest
 class SolverTest(unittest.TestCase):
     def solve_1(self, input_file_name: str) -> int:
         solver = Solver()
-        cave: Cave = solver.parse_file(input_file_name)
-        number_of_sand_falls = cave.simulate_sand_fall()
-        return number_of_sand_falls
+        valve_dict = solver.parse_file(input_file_name)
+        highest_pressure_release = solver.get_highest_pressure_release(valve_dict)
+        return highest_pressure_release
 
     def test_given_1(self):
-        number_of_sand_falls = self.solve_1(GIVEN_FILE_NAME)
-        self.assertEqual(number_of_sand_falls, 24)
+        highest_pressure_release, _ = self.solve_1(GIVEN_FILE_NAME)
+        self.assertEqual(highest_pressure_release, 1651)
 
     def test_input_1(self):
-        number_of_sand_falls = self.solve_1(INPUT_FILE_NAME)
-        self.assertEqual(number_of_sand_falls, 1199)
+        return
+        highest_pressure_release, _ = self.solve_1(INPUT_FILE_NAME)
+        self.assertEqual(highest_pressure_release, 1940)
 
     def solve_2(self, input_file_name: str) -> int:
         solver = Solver()
-        cave: Cave = solver.parse_file(input_file_name)
-        cave.add_bottom()
-        number_of_sand_falls = cave.simulate_sand_fall()
-        return number_of_sand_falls
+        valve_dict = solver.parse_file(input_file_name)
+        highest_pressure_release = solver.get_highest_pressure_release_2(valve_dict)
+        return highest_pressure_release
 
     def test_given_2(self):
-        number_of_sand_falls = self.solve_2(GIVEN_FILE_NAME)
-        self.assertEqual(number_of_sand_falls, 93)
+        return
+        highest_pressure_release = self.solve_2(GIVEN_FILE_NAME)
+        self.assertEqual(highest_pressure_release, 1651)
 
     def test_input_2(self):
-        number_of_sand_falls = self.solve_2(INPUT_FILE_NAME)
-        self.assertEqual(number_of_sand_falls, 23925)
+        return
+        highest_pressure_release = self.solve_1(INPUT_FILE_NAME)
+        self.assertEqual(highest_pressure_release, 1940)
 
 
 unittest.main(exit=False)
